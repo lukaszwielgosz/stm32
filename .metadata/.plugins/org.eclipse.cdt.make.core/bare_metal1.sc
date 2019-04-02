8a944ba404e5d81075f811ae7e8efba349fe2e3c
Łukasz Wielgosz <lukasz.wielgosz9@gmail.com>
2019-04-02 21:43:09 +0200

    initial commit

---
+++ .metadata/.plugins/org.eclipse.cdt.make.core/bare_metal1.sc
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+<?scdStore version="2"?><scannerInfo id="org.eclipse.cdt.make.core.discoveredScannerInfo">
+<instance id="fr.ac6.managedbuild.config.gnu.cross.exe.debug.933787394;fr.ac6.managedbuild.config.gnu.cross.exe.debug.933787394.;fr.ac6.managedbuild.tool.gnu.cross.c.compiler.2097402292;fr.ac6.managedbuild.tool.gnu.cross.c.compiler.input.c.908597415">
+<collector id="org.eclipse.cdt.make.core.PerProjectSICollector"/>
+</instance>
+<instance id="fr.ac6.managedbuild.config.gnu.cross.exe.release.1523570973;fr.ac6.managedbuild.config.gnu.cross.exe.release.1523570973.;fr.ac6.managedbuild.tool.gnu.cross.c.compiler.2057353813;fr.ac6.managedbuild.tool.gnu.cross.c.compiler.input.c.1527683556">
+<collector id="org.eclipse.cdt.make.core.PerProjectSICollector"/>
+</instance>
+</scannerInfo>
---
+++ bare_metal1/Debug/src/main.d
@@ -0,0 +1,22 @@
+src/main.o: ../src/main.c \
+ /home/lukasz/workspace/stm32/bare_metal1/CMSIS/Device/ST/STM32F1xx/Include/stm32f1xx.h \
+ /home/lukasz/workspace/stm32/bare_metal1/CMSIS/Device/ST/STM32F1xx/Include/stm32f103xb.h \
+ /home/lukasz/workspace/stm32/bare_metal1/CMSIS/Include/core_cm3.h \
+ /home/lukasz/workspace/stm32/bare_metal1/CMSIS/Include/core_cmInstr.h \
+ /home/lukasz/workspace/stm32/bare_metal1/CMSIS/Include/cmsis_gcc.h \
+ /home/lukasz/workspace/stm32/bare_metal1/CMSIS/Include/core_cmFunc.h \
+ /home/lukasz/workspace/stm32/bare_metal1/CMSIS/Device/ST/STM32F1xx/Include/system_stm32f1xx.h
+
+/home/lukasz/workspace/stm32/bare_metal1/CMSIS/Device/ST/STM32F1xx/Include/stm32f1xx.h:
+
+/home/lukasz/workspace/stm32/bare_metal1/CMSIS/Device/ST/STM32F1xx/Include/stm32f103xb.h:
+
+/home/lukasz/workspace/stm32/bare_metal1/CMSIS/Include/core_cm3.h:
+
+/home/lukasz/workspace/stm32/bare_metal1/CMSIS/Include/core_cmInstr.h:
+
+/home/lukasz/workspace/stm32/bare_metal1/CMSIS/Include/cmsis_gcc.h:
+
+/home/lukasz/workspace/stm32/bare_metal1/CMSIS/Include/core_cmFunc.h:
+
+/home/lukasz/workspace/stm32/bare_metal1/CMSIS/Device/ST/STM32F1xx/Include/system_stm32f1xx.h:
